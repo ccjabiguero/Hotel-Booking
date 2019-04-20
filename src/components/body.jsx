3369be6edc85b957299f8caa85a6020a97d0c80b
ccjabiguero <jabiguero.catherine@gmail.com>
2019-04-20 11:44:51 +0800

    first commit

---
+++ src/components/body.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import '../css/body.css';
+
+class Body extends Component{
+	render(){
+		return(
+			<div className="footer"></div>
+		);
+	}
+}
+export default Body;
