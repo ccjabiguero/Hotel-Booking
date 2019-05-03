de4b502b1505191c5f547a150a326444d146c7bb
ccjabiguero <jabiguero.catherine@gmail.com>
2019-05-03 14:21:36 +0800

    remove unnecessary code

--- src/components/Guest/SearchGuest.jsx
+++ src/components/Guest/SearchGuest.jsx
@@ -2,41 +2,6 @@ import React from 'react';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 export default class SearchGuest extends React.Component {
-  handleInputChange = event => {
-    const query = event.target.value;
-
-    this.setState(prevState => {
-      const filteredData = prevState.data.filter(element => {
-        return element.name.toLowerCase().includes(query.toLowerCase());
-      });
-
-      return {
-        query,
-        filteredData
-      };
-    });
-  };
-
-  getData = () => {
-    fetch(`http://localhost:4000/restaurants`)
-      .then(response => response.json())
-      .then(data => {
-        const { query } = this.state;
-        const filteredData = data.filter(element => {
-          return element.name.toLowerCase().includes(query.toLowerCase());
-        });
-
-        this.setState({
-          data,
-          filteredData
-        });
-      });
-  };
-
-  componentWillMount() {
-    this.getData();
-  }
-
   render() {
     return (
         <p>Search by First Name
